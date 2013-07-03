c970d8f6c3c0266e1ddca81f0c59cec53ab3f77e
Brice <bricejlin@gmail.com>
2013-07-03 16:11:09 -0400

    added some intro code

--- ruby_ninja.rb
+++ ruby_ninja.rb
@@ -1,4 +1,17 @@
+prompt = ">> "
+ninjas = ["Blue Ninja", "Red Ninja", "Yellow Ninja"]
+
 puts "Welcome to Ruby Ninja!"
 puts
-puts "Please choose your ninja:"
-puts "1. Red Ninja \n 2.Blue Ninja \n 3.Yellow Ninja"
+puts "Please choose the number corresponding to your ninja:"
+
+n = 0
+while n < ninjas.count
+	puts "#{n} #{ninjas[n]}"
+	n += 1
+end
+
+puts
+print prompt; ninja = gets.chomp
+
+puts "Congratulations! You have chosen the #{ninjas[ninja.to_i]}!"
